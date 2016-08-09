45b25a420bb25c2858c3a6841ebe8893e4eb5b19
s15011 <s15011@std.it-college.ac.jp>
2016-08-09 14:29:34 +0900

    テスト

--- app/src/main/java/jp/techinstitute/s15011/productorder/MainActivity.java
+++ app/src/main/java/jp/techinstitute/s15011/productorder/MainActivity.java
@@ -1,7 +1,10 @@
 package jp.techinstitute.s15011.productorder;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = (Button)findViewById(R.id.btnEntry);
+        btn.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, CreateMenber.class);
+                startActivity(intent);
+            }
+        }));
+
+
+/*
+        Button btn = (Button)findViewById(R.id.btnEntry);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.activity_create_menber);
+                //startActivity(new Intent(this, ));
+            }
+        });
+
+        Button btn2 = (Button)findViewById(R.id.btn2);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.activity_chenge_menber_info);
+            }
+        });*/
     }
 }
