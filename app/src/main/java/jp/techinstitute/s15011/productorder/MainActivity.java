0d6215ccf48e70bfdbfc8e38824e626dd993613d
s15004 <s15004@std.it-college.ac.jp>
2016-08-24 10:30:52 +0900

    Merge branch 'master' of https://github.com/s15011/ProductOrder
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/jp/techinstitute/s15011/productorder/MainActivity.java
    #	app/src/main/java/jp/techinstitute/s15011/productorder/MyHelper.java

--- app/src/main/java/jp/techinstitute/s15011/productorder/MainActivity.java
+++ app/src/main/java/jp/techinstitute/s15011/productorder/MainActivity.java
@@ -6,27 +6,122 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    static MyHelper myHelper;
+
+	private class ProductItem {
+		int _id;
+		String id;
+		String name;
+		int price;
+		int stock;
+		String prefecture;
+		boolean flag;
+
+		void setCheckFlag(boolean checkflg) {
+			this.flag = checkflg;
+		}
+
+		boolean getCheckFlag() {
+			return flag;
+		}
+	}
+
+	private List<ProductItem> itemList;
 
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_order_check);
 
-        SQLiteDatabase db = myHelper.getReadableDatabase();
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_order_check);
+
+		MyHelper myHelper = new MyHelper(this);
+		SQLiteDatabase db = myHelper.getWritableDatabase();
+
+		//Cursor cursor = db.query("Columns", new String[]{"id", "name"},null,
+		//      null, null, null, null);
 
 
-        Cursor cursor = db.query(
-                "list_table", new String[] {"_id", "data"},
+		//Cursor cursor = db.query(MyHelper.TABLE_NAME, null, null,null, null, null, null);
+
+		String text = "";
+		String colm = MyHelper.Columns.ID;
+		Cursor cursor = db.query(MyHelper.TABLE_NAME, new String[]{"id"},
+				null, null, null, null, "id DESC");
+		if (!cursor.moveToFirst()) {
+			cursor.close();
+			db.close();
+			return;
+		}
+		int _idIndex = cursor.getColumnIndex(MyHelper.Columns._ID);
+		int idindex = cursor.getColumnIndex(String.valueOf(MyHelper.Columns.ID));
+		int nameindex = cursor.getColumnIndex(MyHelper.Columns.productName);
+		int priceindex = cursor.getColumnIndex(MyHelper.Columns.PRICE);
+		int stockindex = cursor.getColumnIndex(MyHelper.Columns.STOCK);
+		int prefectureindex = cursor.getColumnIndex(MyHelper.Columns.PREFECTURE);
+
+
+		itemList.removeAll(itemList);
+		do {
+			ProductItem item = new ProductItem();
+			item._id = cursor.getInt(_idIndex);
+			item.id = cursor.getString(idindex);
+			item.name = cursor.getString(nameindex);
+			item.price = cursor.getInt(priceindex);
+			item.stock = cursor.getInt(stockindex);
+			item.prefecture = cursor.getString(prefectureindex);
+
+			itemList.add(item);
+
+		} while (cursor.moveToNext());
+
+		Cursor c = db.query(MyHelper.ACCOUNT_TABLE_NAME, new String[]{
+				MyHelper.AccountColumns.firstName,
+				MyHelper.AccountColumns.lastName,
+				MyHelper.AccountColumns.prefectureId,
+				MyHelper.AccountColumns.address,
+				MyHelper.AccountColumns.mailAddress,
+				MyHelper.AccountColumns.password
+		}, null, null, null, null, null);
+
+		if (!cursor.moveToFirst()) {
+			cursor.close();
+			db.close();
+			return;
+		}
+
+
+			TextView textView1 = (TextView) findViewById(R.id.textView4);
+			textView1.setText(text);
+			if (!cursor.moveToFirst()) {
+				cursor.close();
+				db.close();
+				return;
+			}
+
+
+	}
+}
+
+
+
+
+	//myHelper db = new MyHelper(getApplicationContext());
+
+        /*Cursor cursor = db.query(
+                "list_table", new String[] {"ID","productname", "price", "stock","prefecture PREFECTURE"},
                 null, null, null, null, "_id DESC");
+
+
 
         if(!cursor.moveToFirst()){
             cursor.close();
@@ -38,18 +133,24 @@ public class MainActivity extends AppCompatActivity {
         int ProductName = cursor.getColumnIndex(MyHelper.Columns.productName);
         int ProductPrice = cursor.getColumnIndex(MyHelper.Columns.PRICE);
         int ProductStock = cursor.getColumnIndex(MyHelper.Columns.STOCK);
+        int productPrefecture = cursor.getColumnIndex(MyHelper.Columns.PREFECTURE);
 
-        
+        // 6. Cursorを閉じる
+        cursor.close();
+
+        // 7. データベースを閉じる
+        db.close();*/
 
 
-        Button btn = (Button) findViewById(R.id.btnBuy);
+
+        /*Button btn = (Button) findViewById(R.id.btnBuy);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 TextView editView = (TextView) findViewById(R.id.editText);
                 TextView editView2 = (TextView) findViewById(R.id.editText2);
                 TextView editView3 = (TextView) findViewById(R.id.editText3);
-                TextView editView4 = (TextView) findViewById(R.id.editText4);
+               // TextView editView4 = (TextView) findViewById(R.id.editText4);
 
                 TextView book_price = (TextView) findViewById(R.id.textView5);
                 TextView pen_price = (TextView) findViewById(R.id.textView7);
@@ -64,11 +165,11 @@ public class MainActivity extends AppCompatActivity {
                 String a = editView.getText().toString();
                 String b = editView2.getText().toString();
                 String c = editView3.getText().toString();
-                String d = editView4.getText().toString();
+               // String d = editView4.getText().toString();
 
                 String err = "";
 
-                if (a.equals(err)) {
+                /*if (a.equals(err)) {
                     AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
                     builder.setTitle(" errer");
                     builder.setMessage("個数を入力してください。");
@@ -146,11 +247,11 @@ public class MainActivity extends AppCompatActivity {
             }
 
 
-        });
+        });*/
 
 
 
-    }
+    //}
 
 
-}
+//}
